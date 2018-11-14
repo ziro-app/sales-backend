5777ca8d4661b54a0149eaf856aa7fc694038c39
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-11-13 19:34:45 -0800

    add access-control-allow-origin header

--- src/spreadsheet.js
+++ src/spreadsheet.js
@@ -1,5 +1,8 @@
 exports.handler = async (event, context) => {
 	return {
+		headers: {
+			'Access-Control-Allow-Origin', 'https://ziro.online'
+		}
 		statusCode: 200,
 		body: JSON.stringify(event, null, 4)
 	}
