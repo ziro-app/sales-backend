ac7badace2d3c0ee2315c0c9afe0f2f7aa1a8856
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-11-13 19:36:32 -0800

    fix typo

--- src/spreadsheet.js
+++ src/spreadsheet.js
@@ -1,7 +1,7 @@
 exports.handler = async (event, context) => {
 	return {
 		headers: {
-			'Access-Control-Allow-Origin', 'https://ziro.online'
+			'Access-Control-Allow-Origin': 'https://ziro.online'
 		}
 		statusCode: 200,
 		body: JSON.stringify(event, null, 4)
