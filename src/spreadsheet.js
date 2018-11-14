47e8b0f5586b79e051342ae180c6d2c286fe54c6
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-11-13 19:48:03 -0800

    add exact domain to header

--- src/spreadsheet.js
+++ src/spreadsheet.js
@@ -1,7 +1,9 @@
 exports.handler = async (event, context) => {
 	return {
+		/* change Access-Control-Allow-Origin to '*' during development */
 		headers: {
-			'Access-Control-Allow-Origin': 'https://ziro.online'
+			'Access-Control-Allow-Origin': 'https://atendimento.ziro.online'
+			// 'Vary': 'Origin'
 		},
 		statusCode: 200,
 		body: JSON.stringify(event, null, 4)
