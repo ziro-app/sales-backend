d4a85b563ab82ada88b902a1a4abe27e12c84f10
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-11-13 19:56:11 -0800

    add one more needed header

--- src/spreadsheet.js
+++ src/spreadsheet.js
@@ -2,7 +2,8 @@ exports.handler = async (event, context) => {
 	return {
 		/* change Access-Control-Allow-Origin to '*' during development */
 		headers: {
-			'Access-Control-Allow-Origin': 'https://atendimento.ziro.online'
+			'Access-Control-Allow-Origin': 'https://atendimento.ziro.online',
+			'Access-Control-Allow-Headers': 'Content-Type',
 			// 'Vary': 'Origin'
 		},
 		statusCode: 200,
