89a1e76eccd15ffacf9db4ff809b34b373d38eee
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-11-13 20:00:42 -0800

    add Vary header

--- src/spreadsheet.js
+++ src/spreadsheet.js
@@ -1,10 +1,10 @@
 exports.handler = async (event, context) => {
 	return {
-		/* change Access-Control-Allow-Origin to '*' during development */
 		headers: {
-			'Access-Control-Allow-Origin': 'https://atendimento.ziro.online',
+			// 'Access-Control-Allow-Origin': 'https://atendimento.ziro.online',
+			'Access-Control-Allow-Origin': '*',
 			'Access-Control-Allow-Headers': 'Content-Type',
-			// 'Vary': 'Origin'
+			'Vary': 'Origin'
 		},
 		statusCode: 200,
 		body: JSON.stringify(event, null, 4)
