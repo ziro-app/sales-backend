550b83b82a4aa3a3050c7b714e3c3f29f666a77c
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-11-13 21:34:52 -0800

    validate received data and send error message if errors are found

--- src/spreadsheet.js
+++ src/spreadsheet.js
@@ -1,12 +1,31 @@
-exports.handler = async (event, context) => {
-	return {
-		headers: {
-			// 'Access-Control-Allow-Origin': 'https://atendimento.ziro.online',
+try {
+	exports.handler = async ({ httpMethod, queryStringParameters, body }) => {
+		/* check if method, parameters and data are all valid */
+		const methodOk = httpMethod === 'POST'
+		const parametersOk = Object.keys(queryStringParameters).length === 0
+		const { start_date, representative, reseller, transaction_type, end_date } = body
+		const requestOK = methodOk && parametersOK && start_date && representative && reseller
+			&& transaction_type && end_date
+		/* define headers for client response */
+		const headers = {
 			'Access-Control-Allow-Origin': '*',
+			// 'Access-Control-Allow-Origin': 'https://atendimento.ziro.online',
 			'Access-Control-Allow-Headers': 'Content-Type',
-			'Vary': 'Origin'
-		},
-		statusCode: 200,
-		body: JSON.stringify(event, null, 4)
+			'Vary': 'Origin'			
+		}
+		if (requestOK)
+			return {
+				headers,
+				statusCode: 200,
+				body: JSON.stringify(body, null, 4)
+			}
+		else
+			return {
+				headers,
+				statusCode: 405,
+				body: 'Invalid method, parameters or data'
+			}
 	}
+} catch (error) {
+	console.log(error)
 }
