0a44acfb00e244b146cfaa683eba7dc05b14c59e
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-11-19 16:29:51 -0800

    add support for brazilian timezone

--- src/formatDate.js
+++ src/formatDate.js
@@ -15,9 +15,29 @@ const translateMonth = month => {
 	}
 }
 
-const formatDate = date => {
-	dateStr = new Date(date).toString()
+const numberToName = month => {
+	switch (month) {
+		case 01: return 'Jan'
+		case 02: return 'Fev'
+		case 03: return 'Mar'
+		case 04: return 'Abr'
+		case 05: return 'Mai'
+		case 06: return 'Jun'
+		case 07: return 'Jul'
+		case 08: return 'Ago'
+		case 09: return 'Set'
+		case 10: return 'Out'
+		case 11: return 'Nov'
+		case 12: return 'Dez'
+	}
+}
+
+exports.formatDate = date => {
+	const dateStr = new Date(date).toString()
 	return `${dateStr.substr(8,2)}/${translateMonth(dateStr.substr(4,3))}/${dateStr.substr(11,4)}`
 }
 
-module.exports = formatDate
+exports.formatNow = () => {
+	const now = new Date().toLocaleString('pt-br', { timeZone: 'America/Sao_Paulo' })
+	return `${now.substr(0,3)}${numberToName(now.substr(3,3))}${now.substr(5,14)}`
+}
--- src/functions_src/spreadsheet.js
+++ src/functions_src/spreadsheet.js
@@ -1,6 +1,6 @@
-const saveToSheet = require('../saveToSheet')
-const formatDate = require('../formatDate')
 const generateId = require('../generateId')
+const { formatNow, formatDate } = require('../formatDate')
+const saveToSheet = require('../saveToSheet')
 
 try {
 	exports.handler = async ({ httpMethod, queryStringParameters, body }) => {
@@ -16,9 +16,8 @@ try {
 			const { start_date, representative, reseller, transaction_type, end_date } = JSON.parse(body)
 			const requestOk = parametersOk && start_date && representative && reseller && transaction_type && end_date
 			if (requestOk) {
-				const now = new Date().toString() 
 				const atendimento_id = await generateId()
-				const cadastro = `${formatDate(now.substr(4,11))} ${now.substr(16,8)}`
+				const cadastro = formatNow()
 				const atendimento_inicio = formatDate(start_date)
 				const assessor = representative
 				const lojista = reseller
