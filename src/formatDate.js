0c8e522b36772b2adf73c755a7c128456de7316f
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-11-19 16:31:52 -0800

    fix typo

--- src/formatDate.js
+++ src/formatDate.js
@@ -17,18 +17,18 @@ const translateMonth = month => {
 
 const numberToName = month => {
 	switch (month) {
-		case 01: return 'Jan'
-		case 02: return 'Fev'
-		case 03: return 'Mar'
-		case 04: return 'Abr'
-		case 05: return 'Mai'
-		case 06: return 'Jun'
-		case 07: return 'Jul'
-		case 08: return 'Ago'
-		case 09: return 'Set'
-		case 10: return 'Out'
-		case 11: return 'Nov'
-		case 12: return 'Dez'
+		case '01': return 'Jan'
+		case '02': return 'Fev'
+		case '03': return 'Mar'
+		case '04': return 'Abr'
+		case '05': return 'Mai'
+		case '06': return 'Jun'
+		case '07': return 'Jul'
+		case '08': return 'Ago'
+		case '09': return 'Set'
+		case '10': return 'Out'
+		case '11': return 'Nov'
+		case '12': return 'Dez'
 	}
 }
 
