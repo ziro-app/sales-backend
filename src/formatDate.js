4539247cfdc09ecc6dac9fbfbbc96c2636f19c05
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-01-02 22:21:29 -0800

    fix typo

--- src/formatDate.js
+++ src/formatDate.js
@@ -4,7 +4,7 @@ const { formatToTimeZone } = require('date-fns-timezone')
 
 exports.formatDate = date => format(new Date(date), 'DD/MMM/YYYY', { locale: locale })
 
-exports.now = () => formatToTimeZone(new Date('2019-02-02T20:00:00.000Z'), 'DD/MMM/YYYY HH:mm:ss', {
+exports.now = () => formatToTimeZone(new Date(), 'DD/MMM/YYYY HH:mm:ss', {
 		locale: locale,
 		timeZone: 'America/Sao_Paulo'
 	})
