5af20316c3dcad3e6514f868031308ad7ea8bcbd
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-02-10 16:13:22 -0800

    add standard response

---
+++ src/utils/response.js
@@ -0,0 +1,18 @@
+const response = (state) => {
+	const headers = {
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Headers': 'Content-Type',
+		'Vary': 'Origin'
+	}
+	const statusCode = 200
+	const message = {
+		methodError: 'Error. Invalid http method',
+		parametersError: 'Error. Invalid parameters',
+		executionError: 'Error. Internal execution error on runtime. Check logs',
+		ok: 'Success'
+	}
+	const body = JSON.stringify(message[state], null, 4) 
+	return { headers, statusCode, body }
+}
+
+module.exports = response
