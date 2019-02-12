9cf59355e9461725eb3d0e5fa38b7406be3513ce
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-02-11 16:29:50 -0800

    change data to be submitted to spreadhseet based on full completion of the frontend form

--- src/createRow/index.js
+++ src/createRow/index.js
@@ -3,6 +3,7 @@ const { formatDate, now } = require('../utils/formatDate')
 const addRowToSheet = require('./addRowToSheet')
 
 const createRow = async ({
+	is_complete,
 	start_date,
 	representative,
 	reseller,
@@ -16,21 +17,22 @@ const createRow = async ({
 	invoice,
 	comments
 }) => {
+	is_complete = is_complete === "true" ? true : false
 	const atendimento = await generateId()
-	const dataOk = atendimento
+	const dataPartialOk = atendimento
 		&& start_date
 		&& representative
 		&& reseller
 		&& category
 		&& type
 		&& end_date
+	const dataCompleteOk = dataPartialOk
 		&& time
 		&& shipping
 		&& address
 		&& packaging
 		&& invoice
-		&& comments
-	if (dataOk) {
+	if (dataPartialOk) {
 		const cadastro = now()
 		const inicio = formatDate(start_date)
 		const assessor = representative
@@ -39,16 +41,23 @@ const createRow = async ({
 		const tipo = type
 		const despacho = formatDate(end_date)
 		const status = 'Escolhendo'
-		const horario = time
-		const transporte = shipping
-		const endereco = address
-		const fardo = packaging
-		const nota = invoice 
-		const observacao = comments
-		return await addRowToSheet({
-			atendimento, cadastro, inicio, assessor, lojista, categoria, tipo, despacho, status,
-			horario, transporte, endereco, fardo, nota, observacao
-		})
+		if (!is_complete) {
+			return await addRowToSheet({
+				atendimento, cadastro, inicio, assessor, lojista, categoria, tipo, despacho, status
+			})
+		}		
+		if (dataCompleteOk) {
+			const horario = time
+			const transporte = shipping
+			const endereco = address
+			const fardo = packaging
+			const nota = invoice 
+			const observacao = comments
+			return await addRowToSheet({
+				atendimento, cadastro, inicio, assessor, lojista, categoria, tipo, despacho, status,
+				horario, transporte, endereco, fardo, nota, observacao
+			})
+		}
 	}
 	return 'dataError'
 }
