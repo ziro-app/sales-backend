85b6f718ea52cde7086ea809e1e6c23f0e1cb308
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-02-12 19:15:11 -0800

    update calendar insert url

--- src/createRow/addEventToCalendar.js
+++ src/createRow/addEventToCalendar.js
@@ -1,7 +1,7 @@
 const post = require('axios').post
 
 const addEventToCalendar = async (eventData) => {
-	const { data: status } = await post(`${process.env.CALENDAR_URL}`, eventData)
+	const { data: status } = await post(`${process.env.CALENDAR_INSERT_URL}`, eventData)
 	if (status === 'Success')
 		return 'ok'
 	console.log(status)
