e8aa2e682ed23578d7608dfb224f0a8575bf260c
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-02-18 21:06:08 -0800

    fix edge case

--- src/editStatus/index.js
+++ src/editStatus/index.js
@@ -1,10 +1,10 @@
 const editStatusInSheet = require('./editStatusInSheet')
 const removeEventInCalendar = require('./removeEventInCalendar')
 
-const editStatus = async ({ atendimento, status }) => {
-	if (atendimento && status) {
+const editStatus = async ({ atendimento, status, saleIsComplete }) => {
+	if (atendimento && status && saleIsComplete) {
 		const sheetStatus = await editStatusInSheet({ atendimento, status })
-		if (sheetStatus === 'ok' && status === 'Cancelado')
+		if (sheetStatus === 'ok' && status === 'Cancelado' && saleIsComplete === 'true')
 			return await removeEventInCalendar({ atendimento })
 		return sheetStatus
 	}
