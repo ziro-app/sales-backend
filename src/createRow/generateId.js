afb8f000e6b841be551a8afc56052c945e67df2e
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-02-22 13:46:30 -0800

    prevent blank rows to generate errors in generateId function

--- src/createRow/generateId.js
+++ src/createRow/generateId.js
@@ -3,7 +3,7 @@ const { get } = require('axios')
 const generateId = async () => {
 	try {
 		const { data: { values } } = await get(`${process.env.SPREADSHEET_URL}`)
-		const ids = values.map(row => row[1]).slice(1)
+		const ids = values.map(row => row[1] ? row[1] : '').slice(1)
 		return Math.max(...ids) + 1
 	} catch (error) {
 		console.log(error)
